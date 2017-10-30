fa11c74bf97b3dae244819d789cfceeaadbb23d2
James Lopez <jl71491@gmail.com>
2017-10-30 11:11:06 -0700

    Added initial function to render suggestions to page

--- public/main.js
+++ public/main.js
@@ -1,3 +1,6 @@
+const $search = document.querySelector('.search')
+const $dropDown = document.querySelector('#dropDown')
+
 async function elements() {
   const response = await fetch('http://localhost:1337/elements')
   return await response.json()
@@ -12,4 +15,33 @@ elements()
   )
   .catch(error => console.log(error))
 
-console.log(periodicTable)
+const renderResults = string => {
+  if (string === '') {
+    $dropDown.innerHTML = ''
+  } else {
+    $dropDown.innerHTML = ''
+    periodicTable.forEach(element => {
+      const name = element.name.toLowerCase()
+      const symbol = element.symbol.toLowerCase()
+      if (
+        string === name.substr(0, string.length) ||
+        string === symbol.substr(0, string.length)
+      ) {
+        const $results = document.createElement('div')
+        $results.classList.add('suggestions')
+        $results.textContent = element.name + ' ' + element.symbol
+        $dropDown.appendChild($results)
+      }
+    })
+  }
+}
+let accumulator = ''
+
+$search.addEventListener('keydown', event => {
+  if (event.key === 'Backspace') {
+    accumulator = accumulator.substr(0, accumulator.length - 1)
+  } else {
+    accumulator += event.key
+  }
+  renderResults(accumulator)
+})
